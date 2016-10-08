11edde644edc2fe7cd808b99e7495b22d76f2080
Mike Meadows <mmeadow33@gmail.com>
2016-10-07 22:25:45 -0500

    added very very basic styling for inline styling for react

--- public/reactify.js
+++ public/reactify.js
@@ -1,8 +1,18 @@
 socket.on("news", function (data){
   console.log(data);
+
+
+  var divStyle = {
+    color: 'red',
+    background: "lightblue",
+    WebkitTransition: 'all', // note the capital 'W' here
+    msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+  };
+
+
   var HelloWorld = React.createClass({
           render: function() {
-          return <div>
+          return <div style={divStyle}>
               <h1>Hello World from React</h1>
               <h1>this should equal "world" from the socket: {data}</h1>
               <p>Text from REACT</p>
